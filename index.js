236b05934bcad3cd80a3d9073853aa121abacd4f
Wendel <wendel.isaac12@gmail.com>
2023-11-01 17:16:42 -0300

    feat: conexão com o bd e rota get

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require("express")
 const {Pool} = require('pg')
 require('dotenv').config()
 
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL
+})
+
 const app = express()
 
 app.use(express.json())
-app.get('/', (req, res)=> console.log("ola mundo"))
+
+app.get('/alunos', async (req, res) => {
+    try {
+        const {rows} = await pool.query("SELECT * FROM alunos");
+        return res.status(200).send(rows);
+    } catch (error) {
+        console.error('Erro ao buscar alunos:', error);
+        return res.status(500).send('Erro interno do servidor');
+    }
+});
+
 
 app.listen(3000, ()=> console.log("server rodando"))
